4bab01b6b3e7b95e0151c46808cdc5732f1f5ba1
Wilhelm <wilhelmzimmermann8@gmail.com>
2025-05-07 19:53:13 -0300

    opa

---
+++ linked_list/main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next = nullptr;
+
+    Node(int value)
+    {
+        this->value = value;
+    }
+
+    Node(int value, Node *next)
+    {
+        this->value = value;
+        this->next = next;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    LinkedList(): head(nullptr), tail(nullptr), length(0)
+    {
+    }
+
+    LinkedList(int value)
+    {
+        Node *node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    ~LinkedList()
+    {
+        Node *current = head;
+        while (current != nullptr)
+        {
+            Node *nextNode = current->next;
+            delete current;
+            current = nextNode;
+        }
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->value << endl;
+            temp = temp->next;
+        }
+    }
+
+    void append(int value)
+    {
+        Node *node = new Node(value);
+        if (length == 0) {
+            tail = node;
+            head = node;
+            length += 1;
+            return;
+        }
+
+        tail->next = node;
+        tail = node;
+        length += 1;
+    }
+
+    void deleteLast() {
+        if(length == 0) return;
+        if(length == 1) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+            length --;
+            return;
+        }
+
+        Node *oneBeforeLast = head;
+        for(int i = 1; i < length - 1; i++) {
+            oneBeforeLast = oneBeforeLast->next;
+        }
+
+        delete oneBeforeLast->next;
+        oneBeforeLast->next = nullptr;
+        tail = oneBeforeLast;
+        length --;
+    }
+
+    // void deleteLast() {
+    //     if(length == 0) return;
+    //     Node *temp = head;
+    //     if(length == 1) {
+    //         head = nullptr;
+    //         tail = nullptr;
+    //     } else {
+    //         Node *pre = head;
+    //         while(temp->next) {
+    //             pre = temp;
+    //             temp = temp->next;
+    //         }
+    
+    //         tail = pre;
+    //         tail->next = nullptr;
+    //     }
+    //     delete temp;
+    //     length --;
+    // }
+
+    void printHead()
+    {
+        cout << "Head: " << head->value << endl;
+    }
+
+    void printTail()
+    {
+        cout << "Tail: " << tail->value << endl;
+    }
+
+    void printLength()
+    {
+        cout << "Length: " << length << endl;
+    }
+};
+
+int main()
+{
+    LinkedList *myLinked = new LinkedList(4);
+
+    myLinked->append(2);
+    myLinked->append(12);
+    myLinked->append(24);
+    myLinked->append(32);
+    myLinked->printList();
+    myLinked->deleteLast();
+    myLinked->printTail();
+    cout << "------------------" << endl;
+    myLinked->printList();
+
+    delete myLinked;
+    return 0;
+}
