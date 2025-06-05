36ffefbef439e65daffe6cea52ebfc129003a1ec
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2025-06-05 18:15:34 -0300

    feat: insert and selection sorts

--- sorts/main.cpp
+++ sorts/main.cpp
@@ -18,14 +18,66 @@ void bubbleSort(int array[], int size)
     }
 }
 
+void selectionSort(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        if (i != minIndex)
+        {
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
+
+void insertionSort(int array[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int temp = array[i];
+        int j = i - 1;
+        while (j > -1 && temp < array[j])
+        {
+            array[j + 1] = array[j];
+            array[j] = temp;
+            j--;
+        }
+    }
+}
+
 int main()
 {
     int array[] = {6, 4, 2, 5, 1, 3};
+    int array2[] = {4, 2, 6, 5, 1, 3};
+    int array3[] = {4, 2, 6, 5, 1, 3};
     int size = sizeof(array) / sizeof(array[0]);
+    int size2 = sizeof(array2) / sizeof(array2[0]);
+    int size3 = sizeof(array3) / sizeof(array3[0]);
 
     bubbleSort(array, size);
+    selectionSort(array2, size2);
+    insertionSort(array3, size3);
 
     for (auto value : array)
+    {
+        cout << value << " ";
+    }
+    cout << endl;
+    for (auto value : array2)
+    {
+        cout << value << " ";
+    }
+    cout << endl;
+    for (auto value : array3)
     {
         cout << value << " ";
     }
